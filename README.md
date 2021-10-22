fcd688b48043c10bd3133495f12b2822ed7d8654
ferniemann <hello@ferdinand-niemann.eu>
2021-10-22 12:43:15 +0200

    setup

---
+++ README.md
@@ -0,0 +1,32 @@
+# About this Repository
+This is one possible solution for a task I gave to my course at [Coding Bootcamps Europe](https://coding-bootcamps.eu).
+
+## The task
+Your task is to create users for a user database. Each single user is an object (containing the username and their age) within the user database (which is an array). To make this task as convenient and efficient as possible for you, you will be using ``class`` to create each object.
+
+- [ ] First step: Create a function to add a new user to the array, ``if`` both fields (name and age) are filled when the 'Add User' button is clicked. Show the array in your console after each added user.
+- [ ] Second step: Instead of only showing the contents of the array in the console, create a list of all users and their age in your DOM
+
+You can use this as the basis of your HTML:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes Example</title>
+</head>
+<body>
+    <form action="" onsubmit="return false">
+        <label for="input-name">Name:</label>
+        <input type="text" name="" id="input-name" required>
+        <label for="input-age">Age:</label>
+        <input type="number" name="" id="input-age" required>
+        <button id="btn-add">Add User</button>
+    </form>
+    <script src="script.js"></script>
+</body>
+</html>
+```
---
+++ script.js
@@ -0,0 +1,44 @@
+const users = []
+document.getElementById("btn-add").addEventListener("click", addUser)
+
+
+/**
+ * CLASS CONSTRUCTOR FOR THE SINGLE USER OBJECT
+ */
+class User {
+    constructor(userName, userAge) {
+        this.name = userName // this.name is the object's key "name", userName will be the value
+        this.age = userAge // this.age is the object's key "age", userAge will be the value
+    }
+}
+
+/**
+ * FUNCTION: CREATE NEW OBJECT, ADD IT TO THE ARRAY "users" AND RENDER THE ARRAY'S CONTENT
+ */
+function addUser() {
+    const nameField = document.getElementById("input-name")
+    const name = nameField.value
+    const ageField = document.getElementById("input-age")
+    const age = ageField.value
+
+    if (name !== "" && age !== "") { // Condition: Input fields must not be empty
+        const newUser = new User(name, age) // New object with the user's data is created
+        users.push(newUser) // New object is added to the end of the 'users' array
+        console.log(users) // Output of the array in the console
+        renderUsers() // Render function to display the array's contents in the DOM is triggered
+    } else {
+        alert("Bitte fülle beide Felder aus")
+    }
+}
+
+/**
+ * FUNCTION: DISPLAY THE ARRAY'S CONTENTS IN THE DOM
+ */
+function renderUsers() {
+    const list = document.getElementById("list") // the DOM's <ul> element
+    users.forEach(user => { // Itterate through the array
+        const li = document.createElement("li") // create a <li> element for each entry in the array
+        li.innerText = `${user.name}, ${user.age}` // Add the values of the object's keys 'name' and 'age' to the inner Text of the created <li>
+        list.append(li) // Add the created <li> and it's text to the <ul>
+    })
+}
